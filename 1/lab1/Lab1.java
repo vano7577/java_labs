09304c5284a181e4b2a94559adb4c13f2c689168
Vano7577 <43076526+vano7577@users.noreply.github.com>
2020-08-31 16:29:53 +0300

    Update Lab1.java

--- 1/lab1/Lab1.java
+++ 1/lab1/Lab1.java
@@ -1,9 +1,11 @@
 public class Lab1 {
     public static void main(final String[] args) {
-        char s = 0, n = 100, m = 100;
-        for (char a = 0; a <= n; a++) {
-            for (char b = 0; b <= m; b++) {
-                s += (a - b) / (a + 1);
+        char s = 0, i, j, a = 0 , b = 0, n = 100, m = 100;
+        if(a==65535 || b==65535 || n==65535 || m==65535)
+        {System.out.println("can not find char");System. exit(0);}
+        for (i=a ; i <= n; i++) {
+            for (j=b ; j <= m; j++) {
+                s += (i - j) / (i + 1);
             }
         }
         System.out.println(s);
